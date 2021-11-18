94fa8b7d5bfe131c53204216a35315c2138e08ff
JulesSC <81724365+JulesSC@users.noreply.github.com>
2021-11-17 21:41:59 -0500

    added flash

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,1 +1,25 @@
-// import { gsap } from "gsap";
+import { gsap } from "gsap";
+import { GSDevTools } from "gsap/GSDevTools";
+import { MorphSVGPlugin } from "gsap/MorphSVGPlugin";
+
+gsap.registerPlugin(GSDevTools, MorphSVGPlugin);
+MorphSVGPlugin.convertToPath("circle, rect, ellipse, line, polygon, polyline");
+
+const mainTL = gsap.timeline();
+
+function flash(){
+    const tl = gsap.timeline();
+    tl.to("#preloader", {duration: 0.05, backgroundColor: "rgba(0, 0, 0, 1)", ease: "none", delay: .1}, "one");
+    tl.to("#logo", {duration: 0.05, fill: "rgba(255, 255, 255, 1)", delay: .1, ease: "none"}, "one");
+    tl.to("#logo", {duration: 0.05, alpha: 0, ease: "none"});
+    tl.to("#preloader", {duration: 0.05, backgroundColor: "rgba(255, 255, 255, 1)", ease: "none"});
+    tl.to("#preloader", {duration: 0.05, backgroundColor: "rgba(0, 0, 0, 1)", ease: "none"});
+    return tl;
+}
+
+
+
+mainTL.add(flash());
+// .add(stepTwo(), "-=0.15");
+
+GSDevTools.create();
